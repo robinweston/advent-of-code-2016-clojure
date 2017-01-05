fb4d72f2224e0187daf21e0a2b923d18b4ca167a
Robin Weston <lcfcforever@hotmail.com>
2017-01-05 11:07:00 +0000

    Be smarter about calculating decompressed length

--- src/day9/core.clj
+++ src/day9/core.clj
@@ -1,5 +1,5 @@
 (ns day9.core
-(:require [useful-funcs :refer [str-insert any?]]))
+(:require [useful-funcs :refer [any?]]))
 
 (defn re-pos [re s]
         (loop [m (re-matcher re s)
@@ -16,52 +16,10 @@
             :reps (read-string (nth matches 2))
             :start (key raw-marker) 
             :length (count marker-text)
-            :raw-text marker-text
         }))
 
 (defn- parse-markers [raw-markers]
     (map parse-marker raw-markers))
-
-(defn- capture-marker-sequences [input marker]
-    (->> input
-            (drop (+ (:start marker) (:length marker)))
-            (take (:chars marker))
-            (repeat (:reps marker))
-            flatten
-            (apply str)
-            (assoc marker :seq)
-))
-
-(defn- remove-subs [s start length]
-    (str (subs s 0 start) (subs s (+ start length))))
-
-(defn- apply-marker-sequence [input marker]
-    (let [insertion-point (:start marker)]
-        (-> (remove-subs input insertion-point (+ (:length marker) (:chars marker)))
-            (str-insert (:seq marker) insertion-point))))
-
-(defn- increment-marker-start-positions [markers increment]
-    (map #(update % :start + increment) markers))
-
-(defn- string-increment [marker]
-    (- (count (:seq marker)) (:length marker) (:chars marker)))
-
-(defn- apply-marker-sequences [input markers]
-    (loop [remaining-markers markers decompressed input]     
-        (if (seq remaining-markers)
-            (let [current-marker (first remaining-markers)]
-                (recur 
-                    (increment-marker-start-positions (drop 1 remaining-markers) (string-increment current-marker)) 
-                    (apply-marker-sequence decompressed current-marker))
-            )
-        decompressed
-)))
-
-(defn- apply-markers [input markers]
-    (->> markers
-        (map #(capture-marker-sequences input %))
-        (apply-marker-sequences input)
-))
 
 (defn- markers-overlap? [a b]
     (let [a-start (:start a)
@@ -76,14 +34,15 @@
 (defn- remove-overlapped-markers [markers]
     (remove #(marker-is-overlapped? markers %) markers))
 
-(defn decompress [input]
+(defn- calculate-new-input-length [input markers]
+    (+ (count input)
+        (reduce #(- %1 (:length %2)) 0 markers)
+        (reduce #(+ %1 (* (dec (:reps %2)) (:chars %2))) 0 markers)
+    ))
+
+(defn decompressed-length [input]
     (->> input
         (re-pos #"\(\w+\)")
         (parse-markers)
         (remove-overlapped-markers)
-        (apply-markers input)))
-
-(defn decompression-count [input]
-    (->> input
-        decompress
-        count))
+        (calculate-new-input-length input)))
--- test/day9/core_test.clj
+++ test/day9/core_test.clj
@@ -3,17 +3,14 @@
             [day9.core :refer :all]))
 
 (deftest decompression
-    (testing "decompress"
-        (is (= "ADVENT" (decompress "ADVENT")))
-        (is (= "ABBBBBC" (decompress "A(1x5)BC")))
-        (is (= "ABCBCDEFEFG" (decompress "A(2x2)BCD(2x2)EFG")))
-        (is (= "(1x3)A" (decompress "(6x1)(1x3)A")))
-        (is (= "X(3x3)ABC(3x3)ABCY" (decompress "X(8x2)(3x3)ABCY")))
-        )
     
     (def input (slurp "resources/day9.txt"))
     
-    (testing "compression-count"
-        (is (= 18 (decompression-count "X(8x2)(3x3)ABCY")))
-        (is (= 120765 (decompression-count input)))
+    (testing "decompressed-length"
+        (is (= 6 (decompressed-length "ADVENT")))
+        (is (= 7 (decompressed-length "A(1x5)BC")))
+        (is (= 11 (decompressed-length "A(2x2)BCD(2x2)EFG")))
+        (is (= 6 (decompressed-length "(6x1)(1x3)A")))
+        (is (= 18 (decompressed-length "X(8x2)(3x3)ABCY")))
+        (is (= 120765 (decompressed-length input)))
 ))
